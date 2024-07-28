56e56e575091fbae397439df2edbe3969d2eeeb9
Username0x29A <tomasac.22@gmail.com>
2024-07-28 14:45:19 +0200

    d

--- C++/basics/Classes/constructor.cpp
+++ C++/basics/Classes/constructor.cpp
@@ -8,34 +8,59 @@ class Rectangle {
         int width;
 
     public:
-        //Default constructor
-        Rectangle() {
-            length = width = 1;
-        };
+        //non-default constructor. Sets lenght & width to 1
+        Rectangle() { length = width = 1; };
 
         //Prototype of a non default constructor
+        //Implementation will be outside of the class using
+        //scope resolution operator
         Rectangle(int l, int w);
 
         //Public method for calculating the area
-        int area() { 
-            return this->length * this->width;
-        }
+        //Implementation will be outsie of the class using
+        //scope resolution operator
+        int area();
+        int perimeter();
+
+        //Public getter and setters. Implemented directly in the class definition
+        int getLength() { return length; }
+        void setLength(int l) { length = l; } 
+
+        //Destructor
+        ~Rectangle();
 };
 
-int main() { 
-
-    Rectangle r1 = Rectangle();
-
-    cout << r1.area() << endl; // 1
-
-    Rectangle r2 = Rectangle(10, 20);
-
-    cout << r2.area() << endl; // 200
-    
-
-}
-
+//Implementation of constructors and other methods outside of the class
+//using scope resolution operator (::)
 Rectangle::Rectangle(int l, int w) {
-    this->length = l;
-    this->width = w;
+    length = l;
+    width = w;
 }
+
+int Rectangle::area() {
+    return length * width;
+}
+
+int Rectangle::perimeter() {
+    return 2 * (length + width);
+}
+
+Rectangle::~Rectangle() {}
+
+
+int main() {
+    //This declaration is equivalen to Rectangle r = Rectangle(10,5)
+    Rectangle r(10,5);
+
+    cout << "area of a rectangle " << r.area() << endl;
+    cout << "perimeter of a rectangle " << r.perimeter() << endl;
+
+    r.setLength(20);
+
+    cout << r.getLength() << endl;
+
+    cout << "area of a rectangle " << r.area() << endl;
+    cout << "perimeter of a rectangle " << r.perimeter() << endl;
+    
+}
+
