00a8e9893ac2f8befb884b0152944d8c33eaede0
Username0x29A <tomasac.22@gmail.com>
2025-08-04 16:55:38 +0200

    added insert
    implement quicksort

--- C/include/ArrayADT.h
+++ C/include/ArrayADT.h
@@ -2,32 +2,57 @@
  * ArrayADT abstract data type
  *Supports basic methods such as Display, Search, Sort etc
  ************************************************************/
-#include <stdio.h>
 #define SIZE 15
 #define INSERT_NICE
 #define LINEAR_SEARCH
 #define LINEAR_SEARCH_WITH_SHIFT
 
+//State enum
+typedef enum ArrayADT_State
+{
+    UNSORTED,
+    SORTED
+} State;
+
+typedef enum ArrayADT_Order
+{
+    ORDER_ASCENDING,
+    ORDER_DESCENDING,
+    ORDER_OTHER,
+    ORDER_UNSORTED
+} SortOrder;
+
+typedef enum ArrayADT_Codes
+{
+    ARRAY_FULL,
+} Codes;
+
 //ArrayADT structure
 struct ArrayADT
 {
-    int sorted;
-    int size;   //Maximum number of elements stored
-    int length; //Current number of elements stored
-    int A[SIZE];//Fixed size of SIZE elements
+    State state;
+    int capacity;   //Maximum number of elements stored
+    int length;     //Current number of elements stored
+    int A[SIZE];    //Fixed size of SIZE elements
     
 };
 
 
 //Prototypes of operations supported by an ArrayADT 
-extern void Display(struct ArrayADT*);
-extern void Get(struct ArrayADT*, int);
+extern int Get(struct ArrayADT*, int);
 extern void Append(struct ArrayADT*, int);
-extern void Length(struct ArrayADT*);
-extern void Swap(struct ArrayADT*, int, int);
-extern void Insert(struct ArrayADT*, int, int);
+extern int Length(struct ArrayADT*);
+extern int Swap(struct ArrayADT*, int, int);
+extern int InsertAt(struct ArrayADT*, int, int);
+extern int Insert(struct ArrayADT*, int);
 extern int Delete(struct ArrayADT*, int);
 extern int LinearSearch(struct ArrayADT*, int);
 extern int LinearSearchWithShift(struct ArrayADT*, int);
 extern int BinarySearch(struct ArrayADT*, int);
 extern void BubbleSort(struct ArrayADT*);
+extern void Reverse(struct ArrayADT*);  
+extern int Set(struct ArrayADT*, int, int);
+extern int SortedOrder(struct ArrayADT*);
+extern double Median(struct ArrayADT*);
+extern int Mode(struct ArrayADT*);
+extern void _QuickSort(struct ArrayADT*);
