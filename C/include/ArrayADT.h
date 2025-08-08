bf86f11e71cf3e06db2eb4d25a250b4995042c26
Username0x29A <tomasac.22@gmail.com>
2025-08-08 14:28:15 +0200

    asd

--- C/include/ArrayADT.h
+++ C/include/ArrayADT.h
@@ -24,6 +24,7 @@ typedef enum ArrayADT_Order
 
 typedef enum ArrayADT_Codes
 {
+<<<<<<< Updated upstream
     ARRAY_FULL,
     ARRAY_SORTED,
     ARRAY_UNSORTED,
@@ -32,6 +33,11 @@ typedef enum ArrayADT_Codes
     ARRAY_SAME_ELEMENTS,
     OTHER
 } Code;
+=======
+    ARRAY_FULL
+    
+} Codes;
+>>>>>>> Stashed changes
 
 //ArrayADT structure
 struct ArrayADT
