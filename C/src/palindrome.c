6840e419e2c7ae1fc1342c8407e6d37a9c9ead56
Username0x29A <tomasac.22@gmail.com>
2026-03-05 14:20:22 +0100

    palindrome
    final

--- C/src/palindrome.c
+++ C/src/palindrome.c
@@ -1,37 +1,27 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
 
 void flipCase(char *word, unsigned long len);
 int isPalindrome(char *word);
+void toLower(char *word);
+
 int main()
 {
 
-    static char word[] = { "saippuakivikauppias" };
+    static char word[] = { "A mian, a plan, a canal: Panama" };
     
-    isPalindrome(word);
-    unsigned long wordLen = strlen(word) - 1;
-
-    //flipCase(word, wordLen);
-
-
-   int isPalindrome = 1;
+    if(isPalindrome(word))
+    {
+        printf("The word %s is a palindrome\n", word);
+    } else {
+        printf("The word %s is not a palindrome\n", word);
+    }
    
 
-   for(int i = 0, j = wordLen; i < wordLen; i++, j--)
-   {
-        if(!(word[i] == word[j]))
-        {
-            isPalindrome = 0;
-            break;
-        }
-   }
-
-   if(isPalindrome)
-        printf("Word %s is a palindrome\n", word);
-
-   if(!isPalindrome)    
-        printf("Word %s is not a palindrome\n", word);
+    return 0;
+        
 
 }
 
@@ -45,32 +35,67 @@ void flipCase(char *word, unsigned long len)
 
 int isPalindrome(char *word)
 {
-    int _isPalindrome = 0;
-    unsigned long len = strlen(word);
 
-    printf("Word %s of length %lu\n", word, len);
-    printf("1st character is %c. Last character is %c\n", word[0], word[len-1]);
+    int _isPalindrome;
+    unsigned long i,j;
+    j = strlen(word) - 1;
+    _isPalindrome = i = 0;
+    
 
-    int i,j;
-    i = 0;
-    j = len;
+    //translate to common case (lower) first
+    //ASCII range [97-122]
+    toLower(word);
 
-    while(i != j)
+    printf("Transaled word %s\n", word);
+    printf("1st character is %c\nLast character is %c\n", word[i], word[j]);
+
+    while(i < j)
     {
-        //From the left
-        if((word[i] >= 65 || word[i] <= 90) || (word[i] >= 97 || word[i] <= 122))
+        //Both characters are letters
+        if((word[i] >= 97 && word[i] <= 122) && (word[j] >= 97 && word[j] <= 122))
         {
-            //From the right
-            if((word[j] >= 65 || word[j] <= 90) || (word[j] >= 97 || word[j] <= 122))
-            {
-                if(word[i] != word[j])
-                    break; // Both are alphanumeric, but they differ. Not a palindrome
-            }
+            //But they do not match
+            if(word[i] != word[j])
+                return 0;
+            
+            //They match
+            ++i;
+            --j;
+        }
 
-            i++;
-            j--;
+        //Both characters are not letters
+        if((word[i] < 97 || word[i] > 122) && (word[j] < 97 || word[j] > 122))
+        {
+            //Skip them
+            ++i;
+            --j;
+        }
+
+        //Character from the left is a letter but character from the right is not
+        if((word[i] >= 97 && word[i] <= 122) && (word[j] < 97 || word[j] > 122))
+        {
+            //Ingore charcter from the right in this iteration
+            --j;
+        }
+
+        //Character from the left is not a letter but character from the right is
+        if((word[i] < 97 || word[i] > 122) && (word[j] >= 97 || word[j] <= 122))
+        {
+            //Ingore character from the left in this iteration
+            ++i;
         }
     }
 
 
+    return _isPalindrome = 1;
+}
+
+void toLower(char *word)
+{
+    unsigned long _len = strlen(word);
+
+    for(unsigned long i = 0; i < _len; i++)
+    {
+        word[i] = word[i] | 32;
+    }
 }
